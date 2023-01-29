c04c4571071aa922b52f37839533362fc7fdf9bb
NazariiYovenko <nazarjovenko@gmail.com>
2023-01-29 18:24:05 +0200

    check storage

--- src/js/get-film-card.js
+++ src/js/get-film-card.js
@@ -1,21 +1,21 @@
 import FetchFilms from './fetch-films';
+import loadFromStorage from './load-from-storage';
 
 export default function getFilmCard(query = '', page = 1) {
   const fetch = new FetchFilms();
-  // const getLoadedFilms = localStorage.getItem('CURRENT_FILMS');
-  // if (getLoadedFilms) {
-  //   return getLoadedFilms;
-  // }
-  //  Зробити завантаження з локал сторадж
-  // Якщо не False то робимо раннє повернення
-  loadFromStorage(query, page);
+  const filmsInStorage = loadFromStorage(query, page);
+  if (filmsInStorage) {
+    console.log('Load from storage');
+    console.log(filmsInStorage);
+    return Promise.resolve(filmsInStorage.results);
+  }
 
   return fetch.fetchFilms(query, page).then(data => {
     try {
       const filmsToSave = JSON.stringify({
         page: page,
         query: query,
-        results: data.results,
+        results: data,
       });
       localStorage.setItem('CURRENT_FILMS', filmsToSave);
     } catch (error) {
@@ -23,23 +23,4 @@ export default function getFilmCard(query = '', page = 1) {
     }
     return data;
   });
-}
-
-function loadFromStorage(query, page) {
-  console.log(query, page);
-  try {
-    const getLoadedFilms = localStorage.getItem('CURRENT_FILMS');
-    // Розпарсити дані якщо такі є і порівняти сторінку і пошуковий запит
-    // Якщо однакові з поточними вернути результат,якщо ні то порожні дані
-    const parseLoadedFilms = JSON.parse(getLoadedFilms);
-    // const loadedParsedFilms = loadedForParseFilms.find(array => array.query && array.page  ===)
-    console.log(parseLoadedFilms);
-    if (parseLoadedFilms.page === page && parseLoadedFilms.query === query) {
-      return parseLoadedFilms.results;
-      console.log(parseLoadedFilms.results);
-    }
-    return parseLoadedFilms;
-  } catch (error) {
-    console.error('Get state error: ', error.message);
-  }
 }
