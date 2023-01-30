1d08100a06e3aace142b8c3b87c592d9b8590929
NZAlona <amakoveychenko@gmail.com>
2023-01-30 22:26:45 +0200

    adds verification when release-date is not specified

--- src/markup/markupfilmcard.js
+++ src/markup/markupfilmcard.js
@@ -38,11 +38,18 @@ export function createMarkup(resp) {
   const filmCard = resp
     .map(({ poster_path, title, genre_ids, release_date, id }) => {
       getGenresName(genre_ids);
-      const releaseDate = release_date.slice(0, 4);
+      let releaseDate = '';
+      if (!release_date) {
+        const message = 'N/A';
+        releaseDate = message;
+      }
+      if (release_date) {
+        releaseDate = release_date.slice(0, 4);
+      }
+
       const genreArrayShort = genre_ids.slice(0, 3);
 
-      if (release_date) {
-        return `<li class="film-list__item" data-id=${id}>
+      return `<li class="film-list__item" data-id=${id}>
             <a href="#" class="film-list__link">
               <img class="film-list__img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title}" />
               <div class="film-list__box">
@@ -51,7 +58,6 @@ export function createMarkup(resp) {
               </div>
             </a>
           </li>`;
-      }
     })
     .join('');
 
