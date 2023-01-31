b2bd14a09724697f064def41ce44f7850820aa65
DeadlyPants11 <100023619+DeadlyPants11@users.noreply.github.com>
2023-01-31 11:02:19 +0000

    Deploying to gh-pages from @ DeadlyPants11/project@d8eeabcf72d33f6caed89f58b159f1bca70a34ad 🚀

--- library.b1d8faea.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".loader");const e=document.querySelector(".preloader-wrapper");window.addEventListener("load",(function(){e.classList.add("visually-hidden"),setTimeout((()=>{e.style.display="none"}),400)}));
-//# sourceMappingURL=library.b1d8faea.js.map
--- library.f5897b38.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".loader");var e=document.querySelector(".preloader-wrapper");window.addEventListener("load",(function(){e.classList.add("visually-hidden"),setTimeout((function(){e.style.display="none"}),400)}))}();
-//# sourceMappingURL=library.f5897b38.js.map
